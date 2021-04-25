4ea89dcc7d386831599dd5fdecc88eec46df5163
elliottmotson <78193700+elliottmotson@users.noreply.github.com>
2021-04-25 17:06:06 +0100

    Update test.py

--- python-nmap-0.6.4/nmap/test.py
+++ python-nmap-0.6.4/nmap/test.py
@@ -1,6 +1,5 @@
 import nmap3
 import json
 nmap = nmap3.Nmap()
-results = nmap.nmap_version()
-parse = json.loads(results)
-print(results['service']['ostype'])
+parse = json.loads(nmap.nmap_version())
+print(parse['service']['ostype'])
