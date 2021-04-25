f4bb460908e6570585215493b33efbf92a6a5c80
elliottmotson <78193700+elliottmotson@users.noreply.github.com>
2021-04-25 23:25:02 +0100

    Update test.py

--- python-nmap-0.6.4/nmap/test.py
+++ python-nmap-0.6.4/nmap/test.py
@@ -4,4 +4,3 @@ nmap = nmap3.Nmap()
 results = nmap.nmap_version()
 
 print(json.loads(results)["nmap"][1])
- {'nmap': (7, 70), 'compiled_with': ('liblua-5.3.3', 'openssl-1.1.1d', 'libssh2-1.8.0', 'libz-1.2.11', 'libpcre-8.39', 'libpcap-1.8.1', 'nmap-libdnet-1.12', 'ipv6'), 'compiled_without': ('',), 'nsock_engines': ('epoll', 'poll', 'select')}
