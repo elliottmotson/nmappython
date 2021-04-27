21894ee828a248475e8c2b0cc4ffe149942b2541
elliottmotson <78193700+elliottmotson@users.noreply.github.com>
2021-04-27 16:16:33 +0100

    Update test.py

--- python-nmap-0.6.4/nmap/test.py
+++ python-nmap-0.6.4/nmap/test.py
@@ -3,7 +3,7 @@ import json
 import re
 from datetime import datetime
 
-nmap3 = nmap3.Nmap()
+nmap = nmap.Nmap()
 results = nmap.nmap_version()
 
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
