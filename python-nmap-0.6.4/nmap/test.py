7d78011cd2de2d6f02f30d553e874b8c5eb93856
elliottmotson <78193700+elliottmotson@users.noreply.github.com>
2021-04-28 01:32:28 +0100

    Update test.py

--- python-nmap-0.6.4/nmap/test.py
+++ python-nmap-0.6.4/nmap/test.py
@@ -43,9 +43,10 @@ def portScan(targetIP):
     if inputCase == "1":
         print("Fingerprinting OS of " + targetIP)
         os_results = nmap.nmap_os_detection(targetIP)
-        print(os_results)
+        #print(os_results)
 
         for targetIP in os_results:
+            print(targetIP)
             osName = targetIP["osmatch"]
             osName = osName[0]
             osName = osName[".name"]
